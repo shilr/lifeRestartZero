70090ba9937217c46446a0b30177a7b3be0e50a8
shilr <45729764+shilr@users.noreply.github.com>
2025-01-22 00:12:34 +0800

    Add files via upload

---
+++ vue-XiuXianGame/src/plugins/router.js
@@ -0,0 +1,82 @@
+import map from '../views/mapExploration.vue';
+import boss from '../views/bossPage.vue';
+import home from '../views/homePage.vue';
+import index from '../views/indexPage.vue';
+import explore from '../views/explorePage.vue';
+import cultivate from '../views/cultivatePage.vue';
+import endlesstower from '../views/endlessPage.vue'
+import game from '../views/game/game.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+    {
+        path: '/',
+        name: 'index',
+        meta: {
+            keepAlive: false
+        },
+        component: index
+    },
+    {
+        path: '/home',
+        name: 'home',
+        meta: {
+            keepAlive: false
+        },
+        component: home
+    },
+    {
+        path: '/cultivate',
+        name: 'cultivate',
+        meta: {
+            keepAlive: false
+        },
+        component: cultivate
+    },
+    {
+        path: '/map',
+        name: 'map',
+        meta: {
+            keepAlive: false
+        },
+        component: map
+    },
+    {
+        path: '/explore',
+        name: 'explore',
+        meta: {
+            keepAlive: false
+        },
+        component: explore
+    },
+    {
+        path: '/boss',
+        name: 'boss',
+        meta: {
+            keepAlive: false
+        },
+        component: boss
+    },
+    {
+        path: '/endlesstower',
+        name: 'endlesstower',
+        meta: {
+            keepAlive: false
+        },
+        component: endlesstower
+    },
+    {
+        path: '/game',
+        name: 'game',
+        meta: {
+            keepAlive: false
+        },
+        component: game
+    },
+];
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
+
+export default router;
